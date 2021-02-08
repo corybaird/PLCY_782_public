7df6fa0b5e8e0f3236eefeafb6c11cc57625618e
Cory <31351628+corybaird@users.noreply.github.com>
2021-02-08 11:02:52 -0500

    add assignment template

---
+++ Assignments/PS_1/PS1_Assignment_Template.R
@@ -0,0 +1,71 @@
+#############################
+
+##  PLCY6782, Spring 2021 PS1  ##
+
+#############################
+
+#You should use this template to complete the problem set
+
+##Name: 
+
+
+# 1.Provide a table of means and standard deviations of the following variables  
+
+# 1.1 total household size:
+# Hint: Groupby and count functions
+
+
+# 1.2 number of children under 18 in the household: 
+# Hint: Groupby and count functions
+
+
+# 1.3 proportion of households with a toilet ()
+# Hint: Groupby and summarise
+
+
+# 1.4 proportion with a separate room for sleeping
+# Hint: Groupby and summarise
+
+
+# 1.5 and proportion using firewood for cooking
+# Hint: Groupby and summarise
+
+
+# 2.2. Now we will study the characteristics of the (declared) head of the household.  
+# Provide a table of means and standard deviations of the following variables for the household heads:
+
+## 2.1 average age
+
+
+## 2.2 proportion male
+
+
+## 2.3 average education level
+
+
+## 2.4 proportion who are working and average earnings in the past year.
+
+
+# 3. Now we will study the characteristics of children in the household between the ages of 6 and 18. 
+## 3.A Filter ages
+
+
+## 3.1 Make a graph of the proportion of children who attend school by age and by gender. 
+
+
+
+## 3.2 Now make a graph of the proportion of children who report working by age and by gender.
+
+
+
+# 4. In Mexico, many live in extended families for instance where children, parents and grandparents may live within the same household.  
+# Using the variable LS05, carry out a short analysis of the different types of households that exist in Mexico. 
+
+
+# 4.1 What proportion are households are composed only of household head, spouse and children? 
+
+
+## 4.2 Do most children live in these type of households or in extended families?  Explain your findings. 
+
+
+
